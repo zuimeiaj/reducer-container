3265f0feb46c6fdb8330b940d469c3091579a045
姚军 <yaojun@chinafintech.cn>
2017-04-01 12:12:41 +0800

    增加reducerContainer 目的

--- libs/container.js
+++ libs/container.js
@@ -1,5 +1,47 @@
 var immutable =require ("immutable")
 var ReducerHooks =require( "./hook");
+/**
+ *
+ * @constructor
+ *
+ * 一开始用react-redux 会这么用:
+ * login.js:
+ * const SHOW_ACTION_NAME="NAMESPACE_SHOW_ACTION_NAME";
+ * export const showActionName=(name)=>({name,type:SHOW_ACTION_NAME});
+ * export const initialState=()=>({name:""});
+ * export const reducer (action,state){
+ *  switch(action.type){
+ *      case SHOW_ACTION_NAME:
+ *                      // do sth..
+ *                      return newState;
+ *  }
+ *
+ * }
+ *
+ * mapDispatchToProps({showActionName,...})
+ * mapStateToProps({...initialState()});
+ *
+ * 如果你觉得使用其他编译器生成该代码模板，或者 copy&paste，
+ * 你也可能会单独建一个action 文件用于存放所有的常量
+ * 或者你认为该代码更清晰，更容易组织程序结构，方便维护，或者......
+ * 但是这都很麻烦，本来只是一个更新页面一个状态，就要写三个玩意~~
+ *
+ * 那么现在：
+ *  class LoginReducer extends ReducerContainer{
+ *      store={
+ *          name:""
+ *      }
+ *      showActionName(name){
+ *          return this.getState().set("name",name);
+ *      }
+ *
+ *  }
+ * export default new LoginReducer();
+ *
+ * 事情还没有这么简单，你还需要自己实现store 和 reducer的连接，componet 和 reducer的连接
+ *  通常在加载路由时来做这些事情
+ *
+ */
 function ReducerContainer(){
     this.globalStore = null;
     this.uniqueKey   = null;
